cd678020030c9528332d32d93b5956b535bf2597
nawal rahmate <nawal.rahmate@usmba.ac.ma>
2021-06-21 14:55:37 +0100

    my first commit

---
+++ src/test/java/com/umanis/afnor/AfnorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.umanis.afnor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AfnorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
